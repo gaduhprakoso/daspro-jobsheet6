4ecb8e68fd7a6d88f761738078bf58b0eb1586a1
gaduhprakoso <gaduhprakoso@gmail.com>
2024-10-02 23:24:35 +0700

    commit ketiga

--- Pemilihan2Percoban110.java
+++ Pemilihan2Percoban110.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Pemilihan2Percoban110{
     public static void main (String[]args){
 
-    Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-    System.out.print("Masukan Tahun anda: ");
-    int tahun = input.nextInt();
+        System.out.print("Masukan Tahun anda: ");
+        int tahun = input.nextInt();
 
-    if ((tahun % 4) == 0){
-        if ((tahun % 100)!= 0){
-            System.out.println("Tahun Kabisat");
-        }else {
-            System.out.println("Bukan Tahun Kabisat"); 
-        }
-    }else{
-        System.out.println("Bukan Tahun Kabisat");
+        if ((tahun % 4) == 0){
+            if ((tahun % 100)== 0){
+                if ((tahun % 400) == 0){
+                    System.out.println("Tahun "+tahun+" adalah Tahun Kabisat");
+                }else
+                    System.out.println("Tahun "+tahun+" Bukan Tahun Kabisat"); 
+            }else
+                System.out.println("Tahun "+tahun+" Bukan Tahun Kabisat"); 
+        }else
+            System.out.println("Tahun "+tahun+" Bukan Tahun Kabisat");
     }
-}
 }
